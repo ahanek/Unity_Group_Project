df06210cc21501293c9a539f80ea618ba97417af
Ethan <emohr2@emich.edu>
2022-03-24 17:20:20 -0400

    Created PowerUp prefab, along with powerup and player scripts.

---
+++ PvP/Assets/Scripts/Player.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    /*
+    void OnTriggerEnter(Collider other)
+    {
+        Transform rootT = other.gameObject.transform.root;
+        GameObject go = rootT.gameObject;
+        if (go.tag == "SpeedBoost")
+        {
+            //if player touches speed boost...
+            AbsorbPowerUp(go);
+        }
+        else
+        {
+            print("not triggered by power-up: " + go.name);
+        }
+    }
+    public void AbsorbPowerUp(GameObject go)
+    {
+        PowerUp pu = go.GetComponent<PowerUp>();
+        switch (pu.type)
+        {
+            // Leave this switch block empty for now.
+        }
+        pu.AbsorbedBy(this.gameObject);
+    }*/
+
+}
